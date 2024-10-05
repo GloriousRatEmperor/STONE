4ea8cd115ef47749add36bd63081f83557016752
GloriousRatEmperor <franta.stehlik.cz@gmail.com>
2024-10-05 14:58:29 +0200

    ...

--- src/main/java/util/Variables.java
+++ src/main/java/util/Variables.java
@@ -3,9 +3,9 @@ package util;
 import static jade.Window.getScene;
 
 public class Variables {
-    public static int startBlood = 500000;
-    public static int startRock = 500000;
-    public static int startMagic = 500000;
+    public static int startBlood = 50;
+    public static int startRock = 50;
+    public static int startMagic = 50;
 
 
     public static void start() {
